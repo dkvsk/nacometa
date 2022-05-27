76bad09b4cbf6c3b25bb153fa98819962494ea66
Daria Diakovska <dashenka.d12@yandex.ru>
2022-05-27 16:32:07 +0200

    create support and feedback sections

--- app/js/main.js
+++ app/js/main.js
@@ -9,19 +9,46 @@ $(function () {
     return false;
   });
 
-  $(window).on("resize", function (){
+  $(window).on("resize", function () {
     if ($(window).width() > 991) {
-      $(".header__nav").removeAttr("style")
+      $(".header__nav").removeAttr("style");
     }
-  })
+  });
 
   /**
    * init select lang
    */
 
-  $('.js-lang').select2({
-    minimumResultsForSearch: -1
+  $(".js-lang").select2({
+    minimumResultsForSearch: -1,
   });
-    
-});
 
+  /**
+   * init feedback slider
+   */
+
+  const swiper = new Swiper(".swiper.js-feedback-slider", {
+    loop: true,
+    slidesPerView: 1,
+    breakpoints: {
+      767: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      991: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
